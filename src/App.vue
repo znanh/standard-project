d3d2024397c8167af739b3ef290f42def7dd5566
znanh <634488675@qq.com>
2025-10-27 10:50:20 +0800

    执行了npx prettier --write .后很多都自动格式化了

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,9 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
-      <nav><RouterLink to="/">Home</RouterLink><RouterLink to="/about">About</RouterLink>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
